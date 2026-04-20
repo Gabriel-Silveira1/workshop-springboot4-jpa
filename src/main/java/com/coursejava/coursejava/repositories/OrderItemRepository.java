58afd5965589b646b76fc84bcb2f53752dacfb3f
Gabriel Silveira <gabriel-silveira1@hotmail.com>
2026-04-20 15:20:01 -0300

    Fix interface OrderItemRepository

--- src/main/java/com/coursejava/coursejava/repositories/OrderItemRepository.java
+++ src/main/java/com/coursejava/coursejava/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.coursejava.coursejava.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.coursejava.coursejava.entities.OrderItem;
+import com.coursejava.coursejava.entities.pk.OrderItemPk;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPk>{
 	
 }
